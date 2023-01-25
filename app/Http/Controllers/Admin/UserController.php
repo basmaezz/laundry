2ea30f007599a6f9be2d517dee3717994253125f
Beaboelezz <Beaboelezz@pnu.edu.sa>
2023-01-26 00:23:11 +0200

    Truncat Delegate

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -155,7 +155,7 @@ class UserController extends Controller
     }
     public function delegates(Request $request)
     {
-
+        Delegate::truncate();
         $delegates=Delegate::with('user')->get();
         dd($delegates);
         return view('dashboard.users.delegates',compact('delegates'));
