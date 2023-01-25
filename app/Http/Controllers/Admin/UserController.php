13d278b04034abf45a657031bc36f592689e5330
Beaboelezz <Beaboelezz@pnu.edu.sa>
2023-01-26 00:34:49 +0200

    AppUser Truncate

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -140,7 +140,7 @@ class UserController extends Controller
 
     public function customers(Request $request){
 
-        AppUser::truncat();
+        AppUser::truncate();
         $customers=AppUser::with('cities')->get();
         dd($customers);
         return view('dashboard.users.customers',compact('customers'));
