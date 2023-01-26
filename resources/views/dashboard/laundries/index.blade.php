89e9c07644f5693df33b5469772fafa78a06e2a7
Beaboelezz <Beaboelezz@pnu.edu.sa>
2023-01-26 11:28:52 +0200

    update laundry blade

--- resources/views/dashboard/laundries/index.blade.php
+++ resources/views/dashboard/laundries/index.blade.php
@@ -18,7 +18,7 @@
                                         <th>المدينه </th>
                                         <th>الحى</th>
                                         <th>ساعات العمل</th>
-{{--                                        <th>Status </th>--}}
+                                       <th>Status </th>
                                         <th>Actions </th>
                                     </tr>
                                     </thead>
@@ -26,7 +26,7 @@
                                     @foreach($subCategories as $subCategory)
                                     <tr>
                                         <td>{{$subCategory->name_ar}}</td>
-                                        {{-- <td>{{$subCategory->city->name_ar}}</td> --}}
+                                        <td>{{$subCategory->city->name_ar}}</td>
                                         <td>{{$subCategory->address}}</td>
                                         @if($subCategory->around_clock !=Null)
                                         <td> طوال اليوم</td>
