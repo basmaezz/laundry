bcabb70d57e7b5ad0f1c34f65294eba099948268
Beaboelezz <Beaboelezz@pnu.edu.sa>
2023-01-31 22:49:37 +0200

    setting Controller

--- app/Http/Controllers/API/SettingController.php
+++ app/Http/Controllers/API/SettingController.php
@@ -39,28 +39,30 @@ class SettingController extends Controller
     /**  public function cities  . */
     public function cities()
     {
-        try {
-            $cities = City::select('id','name_'. App::getLocale() . ' as name')->get();
-            $data=[];
-            $name='name_'.app()->getLocale();
-            foreach ($cities as $k=>$row){
-                $data[$k]=$row;
-                $regions = [];
-                foreach (Region::where('city_id',$row->id)->get() as $item){
-                    $regions[]=[
-                        'id'=>$item->id,
-                        'name'=>$item->$name,
-                    ];
-                }
-
-                $data[$k]['regions']=$regions;
-
-            }
-            return apiResponse("api.success",$data);
-
-        }catch (\PDOException $ex) {
-            return apiResponse("api.expected_error",[],500,500);
-        }
+        $data=City::with('regions')->get();
+        return apiResponse("api.success",$data);
+//        try {
+//            $cities = City::select('id','name_'.App::getLocale().' as name')->get();
+//
+//            $data=[];
+//            $name='name_'.app()->getLocale();
+//            foreach ($cities as $k=>$row){
+//                $data[$k]=$row;
+//                $regions = [];
+//                foreach (Region::where('city_id',$row->id)->get() as $item){
+//                    $regions[]=[
+//                        'id'=>$item->id,
+//                        'name'=>$item->$name,
+//                    ];
+//                }
+//                dd($item);
+//                $data[$k]['regions']=$regions;
+//            }
+//            return apiResponse("api.success",$data);
+//
+//        }catch (\PDOException $ex) {
+//            return apiResponse("api.expected_error",[],500,500);
+//        }
     }
 
     public function regions($id)
