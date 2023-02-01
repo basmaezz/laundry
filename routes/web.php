85155d9068bdd7b25c9b07e5f28980ff8af3feb1
Beaboelezz <Beaboelezz@pnu.edu.sa>
2023-02-02 01:30:22 +0200

    users

--- routes/web.php
+++ routes/web.php
@@ -176,12 +176,14 @@ Route::get('updateDB',function (){
 });
 
 Route::get('getData',function (){
-   \App\Models\OrderDetails::truncate();
-   \App\Models\Order::truncate();
-   \App\Models\OrderTable::truncate();
-   \App\Models\OrderStatusHistory::truncate();
-   \App\Models\OrderAdditional::truncate();
-   $orders=\App\Models\OrderDetails::all();
-    dd($orders);
+//   \App\Models\OrderDetails::truncate();
+//   \App\Models\Order::truncate();
+//   \App\Models\OrderTable::truncate();
+//   \App\Models\OrderStatusHistory::truncate();
+//   \App\Models\OrderAdditional::truncate();
+//   $orders=\App\Models\OrderDetails::all();
+//    dd($orders);
+    $users=\App\Models\AppUser::all();
+    dd($users);
 
 });
