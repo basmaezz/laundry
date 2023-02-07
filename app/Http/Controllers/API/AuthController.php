90abf0ee681660f62bd3f7ad10551c5f41fb1efa
Beaboelezz <Beaboelezz@pnu.edu.sa>
2023-02-08 00:42:44 +0200

    notofications

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -439,27 +439,21 @@ class AuthController extends ApiController
 //        if ($this->request&&$request->file("personal")['image']){
             $user->avatar = uploadFile($request->file("personal")['image'],'users_avatar');
         }
-//        if ($request->file('personal')['nid_image']){
         if(!empty($request->file("nid_image"))) {
             $delegate->id_image = uploadFile($request->file('personal')['nid_image'],'nid_image');
         }
-//        if ($request->file('personal')['medic_check_image']){
         if(!empty($request->file("medic_check_image"))) {
             $delegate->medic_check = uploadFile($request->file('personal')['medic_check_image'],'medic_check');
         }
-//        if ($request->file('car')['front_image']){
             if(!empty($request->file("front_image"))) {
             $delegate->car_picture_front = uploadFile($request->file('car')['front_image'],'car_front');
         }
-//        if ($request->file('car')['back_image']){
         if(!empty($request->file("back_image"))) {
             $delegate->car_picture_behind = uploadFile($request->file('car')['back_image'],'car_back');
         }
-//        if ($request->file('car')['license_image']){
             if(!empty($request->file("license_image"))) {
             $delegate->car_registration  = uploadFile($request->file('car')['license_image'],'car_registration');
         }
-//        if ($request->file('license')['image']){
         if(!empty($request->file("image"))) {
             $delegate->driving_license = uploadFile($request->file('license')['image'],'driving_license');
         }
@@ -700,7 +694,7 @@ class AuthController extends ApiController
         if ($validator->passes()) {
 
             $JwtUser = JWTAuth::toUser();
-            $user    = User::find($JwtUser->id);
+            $user    = AppUser::find($JwtUser->id);
 
             if (!isset($user)){
 
