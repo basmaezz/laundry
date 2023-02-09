2d90e47ad548fa33a58f2548e97dc791b9050994
Beaboelezz <Beaboelezz@pnu.edu.sa>
2023-02-09 19:33:25 +0200

    drop user_id from notifications table

--- database/migrations/2023_02_01_233657_drop_user_id_from_notifications_table.php
+++ database/migrations/2023_02_01_233657_drop_user_id_from_notifications_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('notifications', function (Blueprint $table) {
+            $table->integer('user_id')->nullable()->unsigned()->change();
             $table->foreignId('app_user_id')->after('send');
 
         });
