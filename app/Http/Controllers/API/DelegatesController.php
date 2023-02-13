84316d8e6c67dc1c3263413d00528eb6ceee29c5
Beaboelezz <Beaboelezz@pnu.edu.sa>
2023-02-13 21:55:14 +0200

    Delegate debug

--- app/Http/Controllers/API/DelegatesController.php
+++ app/Http/Controllers/API/DelegatesController.php
@@ -183,9 +183,10 @@ class DelegatesController extends Controller
 
     public function accept_order(Request $request,$order_id){
         $app_user_id = auth('app_users_api')->user()->id;
-        $order = OrderTable::where('id',$order_id)
+//        $order = OrderTable::whereIn('status_id',[OrderController::WaitingForDelivery,OrderController::AcceptedByDelivery,OrderController::WaitingForDeliveryToReceiveOrder])
+        $order = OrderTable::whereIn('status_id',[OrderController::WaitingForDelivery,OrderController::WaitingForDeliveryToReceiveOrder])
+            ->where('id',$order_id)
             ->firstOrFail();
-        dd($order);
         $order->status_id = $order->status_id+1;
         $order->delivery_id = $app_user_id;
         $order->save();
