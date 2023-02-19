84b5b6fb52af74f4ac0e7b77ea9425d341791950
Beaboelezz <Beaboelezz@pnu.edu.sa>
2023-02-19 23:43:39 +0200

    seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,18 +21,18 @@ class DatabaseSeeder extends Seeder
 //             'password'=>'$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi'
 //         ]);
         $this->call([
-//            AppUserSeeder::class,
-//             carTypesSeeder::class,
-//             EducationsLevelSeeder::class,
-//             CitySeeder::class,
+////            AppUserSeeder::class,
+////             carTypesSeeder::class,
+////             EducationsLevelSeeder::class,
+////             CitySeeder::class,
 //            CategorySeeder::class,
-//            subCategorySeeder::class,
-//            CatgeoryItemSeeder::class,
-//            productSeeder::class,
-//            productServiceSeeder::class,
-            FaqSeeder::class,
-            CouponSeeder::class,
-            UserSeeder::class,
+            subCategorySeeder::class,
+////            CatgeoryItemSeeder::class,
+////            productSeeder::class,
+////            productServiceSeeder::class,
+//            FaqSeeder::class,
+//            CouponSeeder::class,
+//            UserSeeder::class,
         ]);
     }
 }
