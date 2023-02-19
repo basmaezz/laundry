7bd03caea1f26611c8159f35ce4ffc85c7683d4d
Beaboelezz <Beaboelezz@pnu.edu.sa>
2023-02-19 22:41:12 +0200

    dbSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,21 +15,21 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         //  \App\Models\User::factory(10)->create();
-//         \App\Models\User::factory()->create([
-//             'name' => 'Test',
-//             'email' => 'admin@admin.com',
-//             'password'=>'$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi'
-//         ]);
+         \App\Models\User::factory()->create([
+             'name' => 'Test',
+             'email' => 'admin@admin.com',
+             'password'=>'$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi'
+         ]);
         $this->call([
-//            AppUserSeeder::class,
-//             carTypesSeeder::class,
-//             EducationsLevelSeeder::class,
+            AppUserSeeder::class,
+             carTypesSeeder::class,
+             EducationsLevelSeeder::class,
              CitySeeder::class,
-//            CategorySeeder::class,
-//            subCategorySeeder::class,
-//            CatgeoryItemSeeder::class,
-//            productSeeder::class,
-//            productServiceSeeder::class,
+            CategorySeeder::class,
+            subCategorySeeder::class,
+            CatgeoryItemSeeder::class,
+            productSeeder::class,
+            productServiceSeeder::class,
         ]);
     }
 }
