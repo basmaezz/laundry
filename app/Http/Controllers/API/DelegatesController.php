e95683cfbe65f97b0e7cd53f6d986d3d63e4d49b
Beaboelezz <Beaboelezz@pnu.edu.sa>
2023-02-25 14:20:27 +0200

    debug histories

--- app/Http/Controllers/API/DelegatesController.php
+++ app/Http/Controllers/API/DelegatesController.php
@@ -243,10 +243,11 @@ class DelegatesController extends Controller
 
     public function delegate_has_order(Request $request){
         $app_user_id = auth('app_users_api')->user()->id;
-        $histories = DeliveryHistory::where('user_id',$app_user_id)->limit(20)->latest()->get();
+        $histories = DeliveryHistory::with('order')->where('user_id',$app_user_id)->limit(20)->latest()->get();
+        dd($histories);
         $count = 0;
         foreach ($histories as $history){
-            dd ($history->order->status_id);
+
             if(
                 ($history->order->status_id == OrderController::AcceptedByDelivery && $history->direction == 'ToLaundry') ||
                 ($history->order->status_id == OrderController::AcceptedByDeliveryToYou && $history->direction == 'FromLaundry')
