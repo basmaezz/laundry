911e9d451bddf3a33d1292a38134cc6ea78b2d0e
Beaboelezz <Beaboelezz@pnu.edu.sa>
2023-02-27 13:54:40 +0200

    debug delegate request

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -389,7 +389,7 @@ class AuthController extends ApiController
 
     public function editProfileDelegate(Request $request)
     {
-
+dd($request->all());
         $user = auth()->user();
 
        $validator          = Validator::make($request->all(), [
@@ -454,6 +454,7 @@ class AuthController extends ApiController
         if (!empty($request->file("personal")['image'])){
             $user->avatar = uploadFile($request->file("personal")['image'],'users_avatar');
         }
+
         if (!empty($request->file("personal")['nid_image'])){
             $delegate->id_image = uploadFile($request->file('personal')['nid_image'],'nid_image');
         }
