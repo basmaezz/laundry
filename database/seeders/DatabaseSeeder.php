7080af9166eef06a1a73bd1378fcf3fe8f093ba6
Beaboelezz <Beaboelezz@pnu.edu.sa>
2023-03-08 15:40:46 +0200

    Modify seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,32 +15,32 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         //  \App\Models\User::factory(10)->create();
-//         \App\Models\User::factory()->create([
-//             'name' => 'Admin',
-//             'email' => 'lanadmin@admin.com',
-//             'password'=>bcrypt('password')
-//         ]);
-//        for($i=2000; $i <= 2023; $i++) {
-//            \App\Models\Year::create([
-//                'name'=>$i,
-//            ]);
-//        }
+        //         \App\Models\User::factory()->create([
+        //             'name' => 'Admin',
+        //             'email' => 'lanadmin@admin.com',
+        //             'password'=>bcrypt('password')
+        //         ]);
+        for ($i = 2000; $i <= 2023; $i++) {
+            \App\Models\Year::create([
+                'name' => $i,
+            ]);
+        }
         $this->call([
-            NationalitySeeder::class
-//            NationalitiySeeder::class,
-//            AppUserSeeder::class,
-//             carTypesSeeder::class,
-//             EducationsLevelSeeder::class,
-//             CitySeeder::class,
-//            CategorySeeder::class,
-//            subCategorySeeder::class,
-//            CatgeoryItemSeeder::class,
-//            productSeeder::class,
-//            productServiceSeeder::class,
-////            FaqSeeder::class,
-//            CouponSeeder::class,
-//            UserSeeder::class,
-//            YearSeeder::class
+            // NationalitySeeder::class
+            //            NationalitiySeeder::class,
+            //            AppUserSeeder::class,
+            //             carTypesSeeder::class,
+            //             EducationsLevelSeeder::class,
+            //             CitySeeder::class,
+            //            CategorySeeder::class,
+            //            subCategorySeeder::class,
+            //            CatgeoryItemSeeder::class,
+            //            productSeeder::class,
+            //            productServiceSeeder::class,
+            ////            FaqSeeder::class,
+            //            CouponSeeder::class,
+            //            UserSeeder::class,
+            //            YearSeeder::class
         ]);
     }
 }
