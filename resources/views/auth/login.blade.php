9584c3e2f345e8833969d6e705aa63af6510e8f0
Beaboelezz <Beaboelezz@pnu.edu.sa>
2023-03-12 14:01:03 +0200

    minimize js file

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,23 +8,21 @@
     <meta name="description" content="CoreUI Bootstrap 4 Admin Template">
     <meta name="author" content="Lukasz Holeczek">
     <meta name="keyword" content="CoreUI Bootstrap 4 Admin Template">
-    <!-- <link rel="shortcut icon" href="assets/ico/favicon.png"> -->
     <title> Admin Dashboard</title>
-    <!-- Icons -->
     <link href="{{asset('assets/admin/css/font-awesome.min.css')}}" rel="stylesheet">
     <link href="{{asset('assets/admin/css/simple-line-icons.css')}}" rel="stylesheet">
     <link href="{{asset('assets/admin/dest/style.css')}}" rel="stylesheet">
 </head>
 
 <body class="">
-<div class="container">
+<div class="container" style="margin-top: 150px">
     <div class="row">
         <div class="col-md-8 m-x-auto pull-xs-none vamiddle">
             <div class="card-group ">
                 <div class="card p-a-2">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
-                    <div class="card-block">
+                    <div class="card-block" >
                         <h1>تسجيل دخول</h1>
                         <p class="text-muted"></p>
                         <div class="input-group m-b-1">
@@ -62,29 +60,10 @@
         </div>
     </div>
 </div>
-<!-- Bootstrap and necessary plugins -->
 <script src="{{asset('assets/admin/js/libs/jquery.min.js')}}"></script>
 <script src="{{asset('assets/admin/js/libs/tether.min.js')}}"></script>
 <script src="{{asset('assets/admin/js/libs/bootstrap.min.js')}}"></script>
-<script>
-    function verticalAlignMiddle()
-    {
-        var bodyHeight = $(window).height();
-        var formHeight = $('.vamiddle').height();
-        var marginTop = (bodyHeight / 2) - (formHeight / 2);
-        if (marginTop > 0)
-        {
-            $('.vamiddle').css('margin-top', marginTop);
-        }
-    }
-    $(document).ready(function()
-    {
-        verticalAlignMiddle();
-    });
-    $(window).bind('resize', verticalAlignMiddle);
-</script>
-<!-- Grunt watch plugin -->
-<script src="//localhost:35729/livereload.js"></script>
+
 </body>
 
 </html>
