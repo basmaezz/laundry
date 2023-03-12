c83e6b6c45824c3ff2e63995858580cbd174c148
Beaboelezz <Beaboelezz@pnu.edu.sa>
2023-03-13 00:41:05 +0200

    orders blade

--- resources/views/dashboard/Orders/WaitingForDeliveryToReceiveOrder.blade.php
+++ resources/views/dashboard/Orders/WaitingForDeliveryToReceiveOrder.blade.php
@@ -4,7 +4,7 @@
         <div class="container-fluid">
             <div class="animated fadeIn">
                 <div class="row">
-                    <div class="col-lg-9">
+                    <div class="col-lg-12">
                         <div class="card">
 
                             <div class="card-header">
