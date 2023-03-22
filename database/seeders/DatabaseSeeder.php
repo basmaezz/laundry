cd5d97cc6962515997f84921138200d93fbe86f9
Beaboelezz <Beaboelezz@pnu.edu.sa>
2023-03-22 15:58:12 +0200

    db seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -26,7 +26,7 @@ class DatabaseSeeder extends Seeder
 //            ]);
 //        }
         $this->call([
-             NationalitySeeder::class,
+//             NationalitySeeder::class,
             //            NationalitiySeeder::class,
 //                        AppUserSeeder::class,
 //                         carTypesSeeder::class,
