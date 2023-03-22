3a79d4fc5bfa55d337ca9d2c68b0827485baec5d
Beaboelezz <Beaboelezz@pnu.edu.sa>
2023-03-22 19:27:13 +0200

    updateOrCreate laundry user

--- app/Http/Controllers/Admin/subCategoryController.php
+++ app/Http/Controllers/Admin/subCategoryController.php
@@ -162,7 +162,7 @@ class subCategoryController extends Controller
         $subcategory->save();
 
         $user=User::where('subCategory_id',$id)->first();
-        $user->updateorcreate([
+        $user->updateOrCreate([
             'name'=>$request->name,
             'last_name'=>$request->last_name,
             'email'=>$request->email,
