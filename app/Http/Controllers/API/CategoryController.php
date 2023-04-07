e4c4283cfa75a7ba96c6d9b0bbd36bcd9ab41f7f
Mahmoud Abdo <vipmaa@gmail.com>
2023-04-07 17:27:05 +0200

    debug distance

--- app/Http/Controllers/API/CategoryController.php
+++ app/Http/Controllers/API/CategoryController.php
@@ -37,7 +37,8 @@ class CategoryController extends Controller
             $range=$subcategory->range;
             $data [] = [
                 'id' => $subcategory->id,
-                //'user' => $user,
+                'user' => $user,
+                'user2' => auth()->user(),
                 //'distanceObject' => $distanceObject,
                 'name' => $subcategory->$name,
                 'address' => $subcategory->address,
