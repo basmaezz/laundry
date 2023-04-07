f64f89ae529948941cdab63c893cadf692499f36
Beaboelezz <Beaboelezz@pnu.edu.sa>
2023-04-08 01:40:43 +0200

    Modify Helper

--- app/Helpers/helpr.php
+++ app/Helpers/helpr.php
@@ -428,7 +428,7 @@ function getDistanceFirst($user, $latitude, $longitude)
            * cos( radians( lng ) - radians(' . $longitude . ') ) + sin( radians(' . $latitude . ') )
            * sin( radians( lat ) ) ) )  AS distance');
 
-    $results = $user->select('*', $raw)->addSelect($raw)->get();
+    $results = $user->select('*', $raw)->addSelect($raw)->orderBy('distance')->get();
 
     return $results;
 }
@@ -438,7 +438,7 @@ function getDistanceFirst1($user, $latitude, $longitude)
            * cos( radians( lng ) - radians(' . $longitude . ') ) + sin( radians(' . $latitude . ') )
            * sin( radians( lat ) ) ) )  AS distance');
 
-    $results = $user->select('*', $raw)->addSelect($raw)->get();
+    $results = $user->select('*', $raw)->addSelect($raw)->orderBy('distance')->get();
 
     return $results->distance;
 }
