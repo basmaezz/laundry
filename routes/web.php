83fee2cfc3afca6f968a0d1eb657efd07dfd7439
Beaboelezz <Beaboelezz@pnu.edu.sa>
2023-04-09 23:59:04 +0200

    get Delegates

--- routes/web.php
+++ routes/web.php
@@ -264,10 +264,9 @@ Route::get('usersGet', function () {
     $users = \App\Models\User::all();
     return $users;
 });
-Route::get('deleteDelegate', function () {
-    $delegate = \App\Models\Delegate::where('id',23)->first();
-     $delegate->delete();
-     return 'deleted';
+Route::get('getDelegates', function () {
+    $delegates = \App\Models\Delegate::with('appUser')->get();
+    return $delegates;
 });
 Route::get('getAppUsers', function () {
     $appUsers = \App\Models\AppUser::all();
