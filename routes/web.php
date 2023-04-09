cd39cf18a1460b5f888659d31b03b5adac0d28e7
Beaboelezz <Beaboelezz@pnu.edu.sa>
2023-04-10 00:42:35 +0200

    Route

--- routes/web.php
+++ routes/web.php
@@ -409,3 +409,10 @@ Route::get('deleteDelegate',function (){
         return 'deleted';
 
 });
+
+
+Route::get('getPone',function (){
+    $phone=DB::table('app_users')->where('mobile',966542882969)->get();
+    return $phone;
+
+});
