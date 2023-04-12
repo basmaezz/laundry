dea7457a283d08b312ab3f5138686b8bb5f83022
Beaboelezz <Beaboelezz@pnu.edu.sa>
2023-04-13 00:32:49 +0200

    debug order

--- app/Http/Controllers/API/OrderController.php
+++ app/Http/Controllers/API/OrderController.php
@@ -298,7 +298,7 @@ class OrderController extends Controller
             $order->save();
 
             $name = 'name_' . App::getLocale();
-dd($order->subCategoriesTrahed->$name);
+dd($order->subCategoriesTrahed->name_ar);
             NotificationController::sendNotification(
                 getStatusName($request->get('status_id')),
                 __('api.order_update', ['laundry' => $order->subCategoriesTrahed->$name, 'status' => getStatusName($request->get('status_id'))]),
