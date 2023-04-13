10757a145caa216a594293b16614d73900f6cf59
Beaboelezz <Beaboelezz@pnu.edu.sa>
2023-04-13 16:19:15 +0200

    debug delegates

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -238,6 +238,7 @@ class UserController extends Controller
             abort(403);
         };
         $delegates=Delegate::with(['appUserTrashed','appUserTrashed.citiesTrashed','nationality'])->get();
+        dd($delegates);
         return view('dashboard.users.delegates',compact('delegates'));
     }
     public function CreateDelegate()
