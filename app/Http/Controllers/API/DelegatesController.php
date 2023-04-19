5b0eae48b2a8a785884c534971c83181f71704a1
Beaboelezz <Beaboelezz@pnu.edu.sa>
2023-04-19 16:16:04 +0200

    modify response

--- app/Http/Controllers/API/DelegatesController.php
+++ app/Http/Controllers/API/DelegatesController.php
@@ -48,9 +48,8 @@ class DelegatesController extends Controller
         }
         $settings=SiteSetting::first();
         $delegate_range=$settings->distance_delegates;
-        dd($delegate_range);
 
-        return apiResponseOrders('api.My_Order', count($data), $data, $delegate_range);
+        return apiResponseOrders('api.My_Order',$delegate_range, count($data), $data);
     }
 
     public function delegate_order_details($order_id, Request $request)
