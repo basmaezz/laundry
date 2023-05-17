87e2f9afac260b5593d13dfc890bd3f6e11aaa2d
Beaboelezz <Beaboelezz@pnu.edu.sa>
2023-05-17 21:22:57 +0300

    modify delegate

--- resources/views/dashboard/users/delegates.blade.php
+++ resources/views/dashboard/users/delegates.blade.php
@@ -1,20 +1,121 @@
+{{--@extends('../layouts.app')--}}
+{{--@section('content')--}}
+{{--    <main class="main" style="margin-top: 25px">--}}
+{{--    <div>--}}
+{{--            <nav aria-label="breadcrumb" class="navBreadCrumb">--}}
+{{--                <ol class="breadcrumb">--}}
+{{--                    <li class="breadcrumb-item"><a href="{{route('dashboard')}}">الرئيسيه</a></li>--}}
+{{--                    <li class="breadcrumb-item active" aria-current="page">المناديب    </li>--}}
+{{--                </ol>--}}
+{{--            </nav>--}}
+{{--            <div class="col-lg-12">--}}
+{{--                <div class="card">--}}
+{{--                    <div class="card-header">--}}
+{{--                        <i class="fa fa-align-justify"></i>--}}
+{{--                        <a href="{{route('delegate.create')}}" class="btn btn-primary" style="float: left;margin-right:5px" >اضافه مندوب</a>--}}
+{{--                        <a href="{{route('delegates.index')}}" class="btn btn-success" style="float: left">تحديث</a>--}}
+
+{{--                    </div>--}}
+{{--                    <div class="card-block">--}}
+{{--                        <table class="table table-striped" id="table_id">--}}
+{{--                            <thead>--}}
+{{--                            <tr>--}}
+                                <th>الرقم التسلسلى</th>
+                                <th>الاسم</th>
+                                <th>المدينه</th>
+                                <th>الجنسيه</th>
+                                <th>نوع التعاقد</th>
+                                <th>رقم الهويه الوطنيه/الاقامه </th>
+                                <th> الحاله</th>
+                                <th>تاريخ الالتحاق </th>
+                                <th>الاجراءات</th>
+{{--                            </tr>--}}
+{{--                            </thead>--}}
+
+{{--                        </table>--}}
+
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--    </main>--}}
+
+{{--@endsection--}}
+{{--@push('javascripts')--}}
+{{--    <script type="text/javascript">--}}
+{{--        $(function() {--}}
+{{--            var table = $('#table_id').DataTable({--}}
+{{--                processing: true,--}}
+{{--                serverSide: true,--}}
+{{--                ajax: "{{ Route('delegates.index') }}",--}}
+{{--                columns: [{--}}
+{{--                    data: 'id',--}}
+{{--                    name: 'id'--}}
+{{--                },{--}}
+{{--                    data: 'name',--}}
+{{--                    name: 'name'--}}
+{{--                },{--}}
+{{--                    data: 'city',--}}
+{{--                    name: 'city'--}}
+{{--                },{--}}
+{{--                    data: 'nationality',--}}
+{{--                    name: 'nationality'--}}
+{{--                },{--}}
+{{--                    data:'request_employment',--}}
+{{--                    name:'request_employment'--}}
+{{--                },{--}}
+{{--                  data: 'id_number',--}}
+{{--                  name:'id_number'--}}
+{{--                }, {--}}
+{{--                    data: 'status',--}}
+{{--                    name: 'status'--}}
+{{--                },{--}}
+{{--                    data:'created_at',--}}
+{{--                    name:'created_at'--}}
+{{--                },{--}}
+{{--                    data: 'action',--}}
+{{--                    name: 'action',--}}
+{{--                    orderable: false,--}}
+{{--                    searchable: false--}}
+{{--                },--}}
+{{--                ]--}}
+{{--            });--}}
+{{--        });--}}
+
+{{--        $('body').on('click', '#deleteBtn', function () {--}}
+{{--            if (confirm("هل تريد اتمام الحذف ؟") == true) {--}}
+{{--                var id = $(this).data('id');--}}
+
+{{--                window.location.reload();--}}
+{{--                $.ajax({--}}
+{{--                    type:"get",--}}
+{{--                    url: "{{ Route('delete.delegate') }}",--}}
+{{--                    data: { id: id},--}}
+{{--                    dataType: 'json',--}}
+{{--                    success: function(res){--}}
+{{--                        var oTable = $('#datatable-crud').dataTable();--}}
+{{--                        oTable.fnDraw(false);--}}
+{{--                    }--}}
+{{--                });--}}
+{{--            }--}}
+{{--        });--}}
+{{--    </script>--}}
+{{--@endpush--}}
 @extends('../layouts.app')
 @section('content')
     <main class="main" style="margin-top: 25px">
-    <div>
+        <div >
             <nav aria-label="breadcrumb" class="navBreadCrumb">
                 <ol class="breadcrumb">
                     <li class="breadcrumb-item"><a href="{{route('dashboard')}}">الرئيسيه</a></li>
-                    <li class="breadcrumb-item active" aria-current="page">المناديب    </li>
+                    <li class="breadcrumb-item active" aria-current="page">الأدمن   </li>
                 </ol>
             </nav>
             <div class="col-lg-12">
                 <div class="card">
                     <div class="card-header">
                         <i class="fa fa-align-justify"></i>
-                        <a href="{{route('delegate.create')}}" class="btn btn-primary" style="float: left;margin-right:5px" >اضافه مندوب</a>
-                        <a href="{{route('delegates.index')}}" class="btn btn-success" style="float: left">تحديث</a>
-
+                        <a href="{{route('user.create')}}" class="btn btn-primary custom" style="float: left; width: 100px; height: 35px " >اضافه أدمن</a>
                     </div>
                     <div class="card-block">
                         <table class="table table-striped" id="table_id">
@@ -39,7 +140,6 @@
             </div>
         </div>
     </main>
-
 @endsection
 @push('javascripts')
     <script type="text/javascript">
@@ -57,39 +157,39 @@
                 },{
                     data: 'city',
                     name: 'city'
-                },{
-                    data: 'nationality',
-                    name: 'nationality'
-                },{
-                    data:'request_employment',
-                    name:'request_employment'
-                },{
-                  data: 'id_number',
-                  name:'id_number'
-                }, {
-                    data: 'status',
-                    name: 'status'
-                },{
-                    data:'created_at',
-                    name:'created_at'
-                },{
-                    data: 'action',
-                    name: 'action',
-                    orderable: false,
-                    searchable: false
                 },
+                    {
+                        data: 'nationality',
+                        name: 'nationality'
+                    }, {
+                        data: 'request_employment',
+                        name: 'request_employment'
+                    }, {
+                        data: 'id_number',
+                        name: 'id_number'
+                    },{
+                       data:'status',
+                        name:'status'
+                    },{
+                      data:'created_at',
+                        name:'created_at'
+                    }, {
+                        data: 'action',
+                        name: 'action',
+                        orderable: false,
+                        searchable: false
+                    },
+
                 ]
             });
         });
-
         $('body').on('click', '#deleteBtn', function () {
             if (confirm("هل تريد اتمام الحذف ؟") == true) {
                 var id = $(this).data('id');
-
                 window.location.reload();
                 $.ajax({
                     type:"get",
-                    url: "{{ Route('delete.delegate') }}",
+                    url: "{{ route('user.delete') }}",
                     data: { id: id},
                     dataType: 'json',
                     success: function(res){
@@ -100,4 +200,5 @@
             }
         });
     </script>
+
 @endpush
