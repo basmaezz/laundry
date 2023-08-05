daca8572c5f4ed87a95c890eb595b74b91f305d8
Beaboelezz <Beaboelezz@pnu.edu.sa>
2023-08-05 15:40:27 +0300

    fix issue

--- resources/views/customers/backEnd/orders/orderDetails.blade.php
+++ resources/views/customers/backEnd/orders/orderDetails.blade.php
@@ -256,7 +256,7 @@
 
 
                                             </tr>
-{{--                                            @endforeach--}}
+                                            @endforeach
                                             </tbody>
                                         </table>
                                     </div>
