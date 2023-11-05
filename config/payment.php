63175eeca2a584992ee5b22d029fff415cfa7677
Mahmoud Abdo <vipmaa@gmail.com>
2023-11-05 15:09:48 +0200

    add apple payment

--- config/payment.php
+++ config/payment.php
@@ -6,6 +6,7 @@ return [
     'EntityID' => [
         'VISA' => env('OPPWA_ENTITY_ID_VISA','8ac7a4c88b670d25018b673bbc1e0064'),
         'MADA' => env('OPPWA_ENTITY_ID_MADA','8ac7a4c88b670d25018b673d9371006c'),
+        'APPLE' => env('OPPWA_ENTITY_ID_APPLE','8ac7a4ca8b95dd52018b9f53ca020515'),
     ],
     'Currency' => env('OPPWA_CURRENCY', 'SAR'),
     'PaymentType' => env('OPPWA_PAYMENT_TYPE', 'DB'),
