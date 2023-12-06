d2cd4a741f20b328b657a25f992417af73214a1f
Beaboelezz <Beaboelezz@pnu.edu.sa>
2023-12-06 22:21:33 +0200

    getCarpetLaundryTimes API

--- app/Http/Controllers/Admin/carpetCategoryController.php
+++ app/Http/Controllers/Admin/carpetCategoryController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\carpetCategory;
 use App\Models\carpetLaundry;
-use App\Models\Subcategory;
+use App\Models\subCategory;
 use Illuminate\Http\Request;
 use Yajra\DataTables\DataTables;
 
