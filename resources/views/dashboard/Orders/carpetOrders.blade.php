5915e1074c7f554a1642a86dbbfbe6740cacb89c
Beaboelezz <Beaboelezz@pnu.edu.sa>
2023-12-17 00:32:47 +0200

    Handle order blades, finish order function

--- resources/views/dashboard/Orders/carpetOrders.blade.php
+++ resources/views/dashboard/Orders/carpetOrders.blade.php
@@ -11,7 +11,7 @@
                             </li>
                             <li class="breadcrumb-item"><a href="{{route('Order.index')}}">الطلبات</a>
                             </li>
-                            <li class="breadcrumb-item active">جدول الطلبات
+                            <li class="breadcrumb-item active">جدول طلبات السجاد
                             </li>
                         </ol>
                     </div>
