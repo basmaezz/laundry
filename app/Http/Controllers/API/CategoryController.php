862fd47320d0a5ad754ad3f8a5c86028247c298c
basmaezz <--global>
2024-02-12 21:30:19 +0300

    Modify Route(names)

--- app/Http/Controllers/API/CategoryController.php
+++ app/Http/Controllers/API/CategoryController.php
@@ -324,4 +324,31 @@ class CategoryController extends Controller
 
         return apiResponse2( $data);
     }
+
+    public function getCarpetLaundries()  {
+        $result = carpetLaundry::get();
+        $data = [];
+        if (count($result)) {
+            $user = auth('app_users_api')->user();
+            foreach ($result as $carpetLaundry) {
+
+                $distance = (!empty($user)) ? getDistanceFirst1($user, $carpetLaundry->lat, $carpetLaundry->lng) : 0;
+
+                $range = $carpetLaundry->range;
+                $data[] = [
+                    'id' => $carpetLaundry->id,
+                    'approximate_duration' => $carpetLaundry->approximate_duration,
+                    'range' => $carpetLaundry->range,
+                    'delivery_price' => $carpetLaundry->delivery_price,
+                    'distance_class' =>  getDistanceClass($distance, $range),
+                    'distance_class_id' =>  getDistanceClassId($distance, $range),
+                ];
+            }
+
+            return apiResponse('api.in_area', $data);
+        } else {
+            return apiResponse('api.out_area', $data);
+        }
+
+    }
 }
